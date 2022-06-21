c6c9315101928951a0a7db3cafc48bbf8e3024d2
William <m983118@gmail.com>
2022-06-21 21:29:56 +0800

    modify: Finished the 02_repeatString
    1. fill the arguments (string, num) in .js
    2. set a variable(initial "") to save result
    3. use for loop to add string to result
    4. set if & else to return num <= -1 condition
    5. "" + "" = ""
    
    issue #00

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num <= -1) {
+        return "ERROR";
+    } 
+    else {
+        let result = "";
+        for (i = 0; i < num; i++) {      
+            result +=  string;            
+        }
+        return result;
+    }
 };
-
 // Do not edit below this line
 module.exports = repeatString;
