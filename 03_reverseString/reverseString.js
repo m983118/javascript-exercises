cef85f829c9d0d9e91242165ebf583ca5c1f6867
William <m983118@gmail.com>
2022-06-22 23:23:32 +0800

    modify : 03_reverseString & 04_removeFromArray
    03_reverseString
    1. set a variable to save alphabet
    2. for loop
    3. use string.length-1 being i initial
    4. until i = 0 , i--
    5. string[i] will be last alphabet to first alphabet
    
    04_removeFromArray
    1. not sure function parameters numbers will be add ...
    2. not sure function parameters numbers could be ued in for of loop
    3. array.indexOf("target") can search specific target and return index
    4. array.splice(index, range) can delete item from index to index+range
    
    issue #00

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(reStr) {
+    let result = "";
+    for (i = reStr.length-1; i >= 0; i--) {
+        result += reStr[i];
+    }
+    return(result);
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...targets) {
+    for (target of targets) {
+        targetIndex = array.indexOf(target);
+        if (targetIndex != -1) {
+            array.splice(targetIndex, 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
